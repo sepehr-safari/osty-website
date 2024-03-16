3c5249c33c348d6c293b8bbe702de70bebd7f95e
Sepehr Safari <safari.sepehr@gmail.com>
2024-03-16 23:45:07 +0330

    fix typo

--- src/features/latest-notes-demo/hooks/index.ts
+++ src/features/latest-notes-demo/hooks/index.ts
@@ -10,7 +10,7 @@ const filters = [
   },
   {
     authors: [
-      'fa984bd7dbb282f07e16e7ae87b26a2a7b9b90b7246a44771f0cf5ae58018f52', // publo
+      'fa984bd7dbb282f07e16e7ae87b26a2a7b9b90b7246a44771f0cf5ae58018f52', // pablo
     ],
     kinds: [1],
     limit: 2,
